5d030beed465686a28999e50730281cc032e2a0e
Christopher Dorst <chris@dorst.email>
2018-06-04 17:40:15 -0500

    Args are default/optional (not needed by Main)

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -5,7 +5,7 @@ namespace Benchmarks
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args = default)
         {
             Run<Tests>();
             WriteReadme();
